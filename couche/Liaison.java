7effe4cc3b3801d1ab77c026fefa65844c15eee8
arous86 <arousseau86@gmail.com>
2023-06-26 16:20:48 -0400

    update

--- couche/Liaison.java
+++ couche/Liaison.java
@@ -1,19 +1,54 @@
 package couche;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.zip.CRC32;
 
 public class Liaison {
-    public static long GenerateCRC(String data) {
-        // Convertir la chaîne en tableau de bytes
-        byte[] bytes = data.getBytes();
-
+    private ArrayList<Trame> trames;
+    private static Liaison instance = null;
+    public static Liaison getInstance() {
+        if (instance == null) {
+            instance = new Liaison();
+        }
+        return instance;
+    }
+    private static long GenerateCRC(byte[] bytes) {
         // Créer une instance de CRC32
         CRC32 crc32 = new CRC32();
 
         // Mettre à jour le CRC32 avec les données
         crc32.update(bytes);
 
-        // Obtenir la valeur CRC calculée
+        // Obtenir la valeur CRC calculée en long
         return crc32.getValue();
     }
+    public byte[] longToBytes(long x) {
+        ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+        buffer.putLong(x);
+        return buffer.array();
+    }
+    public byte[] ConcatenateTrame(Trame trame) {
+        byte[] result = new byte[trame.CRC.length + trame.header.length + trame.data.length];
+        System.arraycopy(trame.CRC, 0, result, 0, trame.CRC.length);
+        System.arraycopy(trame.header, 0, result, trame.CRC.length, trame.header.length);
+        System.arraycopy(trame.data, 0, result, trame.CRC.length + trame.header.length, trame.data.length);
+        return result;
+    }
+    public static void EnvoyerTrames(ArrayList<Trame> _trames) {
+        instance.trames = _trames;
+        for (Trame trame : instance.trames) {
+            long crc = GenerateCRC(trame.data);
+            trame.CRC = instance.longToBytes(crc);
+            byte[] trameBytes = instance.ConcatenateTrame(trame);
+            // TODO: Envoyer trameBytes à la couche physique
+            // TODO: Attendre un ACK
+            // TODO: Si ACK reçu, envoyer la prochaine trame
+            // TODO: Si ACK non reçu, réenvoyer la trame
+            // TODO: Si ACK non reçu après 3 essais, abandonner
+        }
+    }
+
+
+
 }
