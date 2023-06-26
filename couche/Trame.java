d0e511beec4bc1bf6c6ef27de4884a17eed9fb83
SimonCanelle <simon.j-bombardier@hotmail.ca>
2023-06-26 18:39:39 -0400

    ajout crc dans trame

--- couche/Trame.java
+++ couche/Trame.java
@@ -1,7 +1,8 @@
 package couche;
 
 public class Trame{
-    private int TAILLE_FIXE = 20;
-    private byte[] header = new byte[TAILLE_FIXE];
-    private byte[] data;
+    private int TAILLE_FIXE = 6;
+    public byte[] CRC = new byte[64];
+    public byte[] header = new byte[TAILLE_FIXE];
+    public byte[] data;
 }
