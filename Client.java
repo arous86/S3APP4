ab7949e28029434ceedc0405ec5c25be6148314c
arous86 <arousseau86@gmail.com>
2023-06-26 14:45:31 -0400

    update

--- Client.java
+++ Client.java
@@ -1,34 +1,53 @@
 import java.io.*;
 import java.net.Socket;
+import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class Client {
-    public static void main(String[] args) {
+    public static byte[] method(File file)
+            throws IOException
+    {
+
+        // Creating an object of FileInputStream to
+        // read from a file
+        FileInputStream fl = new FileInputStream(file);
+
+        // Now creating byte array of same length as file
+        byte[] arr = new byte[(int)file.length()];
+
+        // Reading file content to byte array
+        // using standard read() method
+        fl.read(arr);
+
+        // lastly closing an instance of file input stream
+        // to avoid memory leakage
+        fl.close();
+
+        // Returning above byte array
+        return arr;
+    }
+    public static void main(String[] args)
+            throws IOException
+    {
         String serverIP = args[0];
         String filePath = args[1];
 
-        BufferedReader reader = null;
 
-        try {
-            FileReader fileReader = new FileReader(filePath);
-            reader = new BufferedReader(fileReader);
+        // Creating an object of File class and
+        // providing local directory path of a file
+        File path = new File(filePath);
 
-            String line;
-            StringBuilder content = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                content.append(line).append("\n");
-            }
+        // Calling the Method1 in main() to
+        // convert file to byte array
+        byte[] array = method(path);
 
-            System.out.println(content.toString());
-        } catch (IOException e) {
-            System.err.println("Error reading the file: " + e.getMessage());
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                System.err.println("Error closing the file: " + e.getMessage());
-            }
-        }
+        // Printing the byte array
+        System.out.print(Arrays.toString(array));
+
+        // Print the name of the file not the path
+        System.out.println(path.getName());
     }
 }
--- Server.java
+++ Server.java
@@ -2,5 +2,5 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    
+
 }
