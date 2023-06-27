0770807911b4b88098ef3b4e0ea8f3710caa0321
Simon Janelle-Bombardier <jans2001@usherbrooke.ca>
2023-06-26 21:00:01 -0400

    wat

--- couche/Transport.java
+++ couche/Transport.java
@@ -2,13 +2,16 @@ package couche;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
+import couche.Trame;
 
 /**
  * descritpion
  *
  */
 public class Transport {
-    private int PAQUET_MAX_LENGHT = 200;
+    private final int PAQUET_MAX_LENGHT = 200;
+    List<couche.Trame> listeTrame;
 
     /**
      * construit les paquets pour l'envoies de fichier
@@ -18,33 +21,33 @@ public class Transport {
      */
     public List<Trame> EnvoyerFichier(String filename, byte[] buffer)
     {
-        List<Trame> listeFrame = new ArrayList<Trame>();
+        listeTrame = new Vector<couche.Trame>();
 
         int numDataPaquet = (buffer.length-1) / (PAQUET_MAX_LENGHT+1); //https://stackoverflow.com/questions/7139382/java-rounding-up-to-an-int-using-math-ceil
 
         //construire premier paquet
         Trame premier = buildFirstPaquet(filename, numDataPaquet);
-        listeFrame.add(premier);
+
+        listeTrame.add(premier);
 
         //construire paquets de données
-        for(Trame tr : buildDataPaquets(buffer, numDataPaquet))
-        {
-            listeFrame.add(tr);
-        }
+        listeTrame.addAll(buildDataPaquets(buffer, numDataPaquet));
 
-        return listeFrame;
+        return listeTrame;
     }
 
     private Trame buildFirstPaquet(String filename, int numpaquets)
     {
+
         return new Trame();
     }
 
-    private List<Trame> buildDataPaquets(byte[] buffer, int numpaquets)
+    private ArrayList<Trame> buildDataPaquets(byte[] buffer, int numpaquets)
     {
         for (int i=0; i<numpaquets; i++)
         {
             Trame temp = new Trame();
+
             //set header
             //set data
         }
