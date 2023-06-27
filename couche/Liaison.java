f828d391a70017d80daf836e177c56230ee8b32e
arous86 <arousseau86@gmail.com>
2023-06-26 22:48:15 -0400

    update

--- couche/Liaison.java
+++ couche/Liaison.java
@@ -1,5 +1,9 @@
 package couche;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.zip.CRC32;
@@ -29,30 +33,87 @@ public class Liaison {
         return buffer.array();
     }
     public byte[] ConcatenateTrame(Trame trame) {
-        byte[] result = new byte[trame.CRC.length + trame.header.length + trame.data.length];
-        System.arraycopy(trame.CRC, 0, result, 0, trame.CRC.length);
-        System.arraycopy(trame.header, 0, result, trame.CRC.length, trame.header.length);
-        System.arraycopy(trame.data, 0, result, trame.CRC.length + trame.header.length, trame.data.length);
-        return result;
+        // concatenate header and data
+        byte[] trameBytes = new byte[trame.header.length + trame.data.length];
+        System.arraycopy(trame.header, 0, trameBytes, 0, trame.header.length);
+        System.arraycopy(trame.data, 0, trameBytes, trame.header.length, trame.data.length);
+        return trameBytes;
     }
     public static void EnvoyerTrames(ArrayList<Trame> _trames, String serverIP, int port) {
         instance.trames = _trames;
         for (Trame trame : instance.trames) {
-            long crc = GenerateCRC(trame.data);
+            byte[] tmp_trameBytes = instance.ConcatenateTrame(trame);
+
+            long crc = GenerateCRC(tmp_trameBytes);
             trame.CRC = instance.longToBytes(crc);
-            byte[] trameBytes = instance.ConcatenateTrame(trame);
+            // concatenate crc first and trame.crc after
+            byte[] trameBytes = new byte[trame.CRC.length + tmp_trameBytes.length];
+            System.arraycopy(trame.CRC, 0, trameBytes, 0, trame.CRC.length);
+            System.arraycopy(tmp_trameBytes, 0, trameBytes, trame.CRC.length, tmp_trameBytes.length);
+
             Physique phys = Physique.getInstance();
-            phys.SendData(serverIP, port, trameBytes);
 
+            int maxTentatives = 3; // Nombre maximum de tentatives pour l'envoi d'une trame
+            int tentative = 0; // Compteur de tentatives
+            boolean isAckReceived = false;
 
-            // TODO: Attendre un ACK
+            while (!isAckReceived && tentative < maxTentatives) {
+                if (phys.SendFrame(serverIP, port, trameBytes)) {
+                    System.out.println("Trame envoyée");
+                    isAckReceived = true;
+                } else {
+                    System.out.println("Trame non envoyée");
+                    tentative++;
+                }
+            }
 
-            // TODO: Si ACK reçu, envoyer la prochaine trame
-            // TODO: Si ACK non reçu, réenvoyer la trame
-            // TODO: Si ACK non reçu après 3 essais, abandonner
+            if (tentative == maxTentatives) {
+                System.out.println("Trame non envoyée après 3 tentatives");
+                // TODO: Abandonner
+            }
         }
+    }
+    public static void RecevoirTrames(byte[] receivedData, ServerSocket serverSocket) throws IOException {
+        byte[] CRC = new byte[8];
+        byte[] header = new byte[6];
+        byte[] data = new byte[receivedData.length - CRC.length - header.length];
+
+        System.arraycopy(receivedData, 0, CRC, 0, CRC.length);
+        System.arraycopy(receivedData, CRC.length, header, 0, header.length);
+        System.arraycopy(receivedData, CRC.length + header.length, data, 0, data.length);
+
+        long crc = GenerateCRC(receivedData);
+        byte[] crcBytes = instance.longToBytes(crc);
+
+        byte[] trameBytes = new byte[header.length];
+        if (CRC.equals(crcBytes)) {
+            // copy the 4 first bytes of header
+            System.arraycopy(header, 0, trameBytes, 0, 4);
+            // place 0 in the 5th byte and 0 in the 6th byte
+            trameBytes[4] = 0;
+            trameBytes[5] = 0;
+        }
+        else {
+            // copy the 4 first bytes of header
+            System.arraycopy(header, 0, trameBytes, 0, 4);
+            // place 1 in the 5th byte and 0 in the 6th byte
+            trameBytes[4] = 1;
+            trameBytes[5] = 0;
+        }
+
+        // Repondre au client
+        Socket clientSocket = serverSocket.accept();
+        DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
+
+        outToClient.writeByte(trameBytes[4]);
+
+        outToClient.flush();
+
+        // Fermeture des flux et des sockets
+        outToClient.close();
+        clientSocket.close();
+
+    }
     }
 
 
-
-}
--- couche/Physique.java
+++ couche/Physique.java
@@ -1,10 +1,11 @@
 package couche;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Physique {
-    // Singleton
     private static Physique instance = null;
     public static Physique getInstance() {
         if (instance == null) {
@@ -13,7 +14,7 @@ public class Physique {
         return instance;
     }
     private static Socket socket;
-    public void SendData(String serverIP, int port, byte[] dataToSend) {
+    public boolean SendFrame(String serverIP, int port, byte[] frameToSend) {
         try {
             if (socket == null || socket.isClosed()) {
                 socket = new Socket(serverIP, port);
@@ -22,7 +23,37 @@ public class Physique {
 
             // Envoi des données au serveur
             OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(dataToSend);
+            outputStream.write(frameToSend);
+
+            // Attente de la réponse du serveur (ACK)
+            InputStream inputStream = socket.getInputStream();
+            byte ack = (byte) inputStream.read();
+            System.out.println("ACK reçu : " + ack);
+            if (ack == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public void ReceiveFrame(int port){
+        try{
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Serveur en attente de connexions...");
+
+            Socket clientSocket = serverSocket.accept(); // Attente de la connexion d'un client
+            System.out.println("Client connecté.");
+
+            // Lecture des données envoyées par le client
+            InputStream inputStream = clientSocket.getInputStream();
+            byte[] receivedData = new byte[256];
+
+            Liaison liaison = Liaison.getInstance();
+            liaison.RecevoirTrames(receivedData, serverSocket);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
